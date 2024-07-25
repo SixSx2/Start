4b401be24026e84ec0c5fd101dd3e20d3326ea35
Rafael <108968570+SixSx2@users.noreply.github.com>
2024-07-25 13:20:48 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+
+function mostrarConteudo(id) {
+    const conteudos = document.querySelectorAll('.conteudo');
+    conteudos.forEach(conteudo => {
+        conteudo.classList.remove('ativo');
+    });
+    document.getElementById(id).classList.add('ativo');
+}
+
+// Função que exibe um aviso
+function mostrarAviso() {
+    alert("Este treino ainda não esta disponivel! Tente novamente na proxima atualização");
+}
+
+// Adiciona um evento de clique ao botão
+document.getElementById("meuBotao").addEventListener("click", mostrarAviso);
+document.getElementById("meuBotao2").addEventListener("click", mostrarAviso);
+document.getElementById("meuBotao3").addEventListener("click", mostrarAviso);
+
